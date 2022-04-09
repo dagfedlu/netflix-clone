f52f01f6b5e2accfa58edeb983c05e5e06be04f3
dagfedlu <dagmawifedlu09@gmail.com>
2022-04-09 12:52:58 +0300

    adds trailers from youtube if the api exists

--- src/Row.js
+++ src/Row.js
@@ -1,4 +1,6 @@
+import movieTrailer from 'movie-trailer'
 import React, { useEffect, useState } from 'react'
+import YouTube from 'react-youtube'
 import axios from './axios'
 import './Row.css'
 
@@ -8,6 +10,7 @@ const base_url = "https://image.tmdb.org/t/p/original/";
 function Row({ title, fetchUrl, isLargeRow }) {
 
   const [movies, setMovies] = useState([]);
+  const [trailerURL, setTrailerUrl] = useState('');
 
   // a snippet of code which runs based on a specific set of variables
   useEffect(() => {
@@ -22,6 +25,29 @@ function Row({ title, fetchUrl, isLargeRow }) {
 
   }, [fetchUrl]);
 
+  //trailers from youtube
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  //when tumbnail is clicked
+  const handleClick = (movie) => {
+    if (trailerURL) {
+      setTrailerUrl('');
+    } else {
+      movieTrailer(movie?.name || '')
+      .then(url => {
+        const urlParams = new URLSearchParams(new URL(url).search);
+        setTrailerUrl(urlParams.get("v"));
+      }).catch(error => console.log(error))
+    }
+  } 
+
   // console.table(movies);
   
   return (
@@ -32,11 +58,13 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies.map(movie => (
             <img
                 key={movie.id}
+                onClick={() => handleClick(movie)}
                 className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
                 src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
                 alt={movie.name}/>
         ))}
         </div>
+        <YouTube videoId={trailerURL} opts={opts} />
     </div>
   )
 }
